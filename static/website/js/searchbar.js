8627a0dfc11a849f35bcaecbcd28999e6ad30619
Kalightortaio <ubuntu@ip-172-31-14-173.us-west-1.compute.internal>
2022-02-03 08:52:35 +0000

    removed static folder

--- static/website/js/searchbar.js
+++
@@ -1,6 +0,0 @@
-(function() {
-    var cx = '001850937032020622434:loofifflmae';
-    var gcse = document.createElement('script'); gcse.type = 'text/javascript'; gcse.async = true;
-    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//www.google.com/cse/cse.js?cx=' + cx;
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
-})();
